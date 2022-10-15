e6aaea9fa27fa2a634ce0037438e3fc0364751b0
Jérôme Ruillier <jerome.ruillier@aronnax.eu>
2022-10-15 08:54:29 -0400

    Javadoc++

--- smartconstraints-annotationprocessor/src/main/java/eu/aronnax/smartconstraints/annotationprocessor/CopyConstraintsProcessor.java
+++ smartconstraints-annotationprocessor/src/main/java/eu/aronnax/smartconstraints/annotationprocessor/CopyConstraintsProcessor.java
@@ -8,6 +8,18 @@ import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
+/**
+ * The entry point of SmartConstraints. This annotation processor will :
+ * <ul>
+ *     <li>look for @CopyConstraints annotations</li>
+ *     <li>scan the package indicated in the 'from' property</li>
+ *     <li>find any entities annotated with validation constraints</li>
+ *     <li>extract objects that describe all validation constraints in entities</li>
+ *     <li>prepare objects that describe what should be generated</li>
+ *     <li>generate as a string one class per entity with all meta annotations within</li>
+ *     <li>write generated classes</li>
+ * </ul>
+ */
 @SupportedAnnotationTypes({"javax.validation.constraints.*", "eu.aronnax.smartconstraints.annotation.CopyConstraints"})
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
 @AutoService(Processor.class)
@@ -27,7 +39,10 @@ public class CopyConstraintsProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        this.processCopyConstraintsAnnotationUC.exec(annotations, roundEnv, this.processingEnv);
+        this.processCopyConstraintsAnnotationUC.exec(annotations, roundEnv,
+
+                this.processingEnv);
+
         return false;
     }
 }
